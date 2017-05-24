97258622f824fcce39e3c47a9a488ca794648250
21303936 <21303936@205-10.ens.fsi>
2017-05-24 11:40:28 +0200

    ajout gestiion commande

---
+++ Prestazon2-ejb/src/java/metier/GestionCommande.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author 21303936
+ */
+@Stateless
+public class GestionCommande implements GestionCommandeLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
